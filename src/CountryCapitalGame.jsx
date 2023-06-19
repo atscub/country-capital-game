6f8279a1a96601232849bec77e57019a2e1180ba
Abraham <abrahamtoledo90@gmail.com>
2023-06-19 11:24:07 +0200

    Refactored using reducer pattern

--- src/CountryCapitalGame.jsx
+++ src/CountryCapitalGame.jsx
@@ -1,30 +1,60 @@
-import React, { useState, useMemo } from "react";
+import React, { useReducer, useEffect } from "react";
+
+const ACTIONS = {
+  INIT: "init",
+  SELECT: "select",
+};
+
+const onInit = (state, data) => {
+  const places = [...Object.keys(data), ...Object.values(data)].sort(
+    (a, b) => 0.5 - Math.random()
+  );
+  return { selected: [], places: places, data: data };
+};
+
+const onSelect = (state, place) => {
+  const { selected, places, data } = state;
+  if (
+    selected.length === 1 &&
+    (data[place] == selected[0] || data[selected[0]] == place)
+  ) {
+    return {
+      ...state,
+      selected: [],
+      places: places.filter((p) => p !== place && p !== selected[0]),
+    };
+  } else if (selected.length > 1) {
+    return { ...state, selected: [place] };
+  } else {
+    // Values should be unique
+    return { ...state, selected: [...new Set([...selected, place])] };
+  }
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.INIT:
+      return onInit(state, action.payload);
+    case ACTIONS.SELECT:
+      return onSelect(state, action.payload);
+  }
+};
 
 export default function CountryCapitalGame({ data }) {
-  const randomizedPlaces = useMemo(() => {
-    return [...Object.keys(data), ...Object.values(data)].sort(
-      (a, b) => 0.5 - Math.random()
-    );
-  }, []);
+  const [{ selected, places }, dispatch] = useReducer(reducer, {
+    selected: [],
+    places: [],
+    data: {},
+  });
 
-  const [places, setPlaces] = useState(randomizedPlaces);
-  const [selected, setSelected] = useState([]);
+  useEffect(() => {
+    dispatch({ type: ACTIONS.INIT, payload: data });
+  }, [data]);
 
   const selectedColor = selected.length === 1 ? "blue" : "red";
 
   const handleSelectPlace = (place) => {
-    if (
-      selected.length === 1 &&
-      (data[place] == selected[0] || data[selected[0]] == place)
-    ) {
-      setSelected([]);
-      setPlaces(places.filter((p) => p !== place && p !== selected[0]));
-    } else if (selected.length > 1) {
-      setSelected([place]);
-    } else {
-      // Values should be unique
-      setSelected([...new Set([...selected, place])]);
-    }
+    dispatch({ type: ACTIONS.SELECT, payload: place });
   };
 
   return (
